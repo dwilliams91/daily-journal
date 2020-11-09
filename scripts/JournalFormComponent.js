9587717c0799150dceefcaa186c78ff0f4c7c999
Your Github User Name <david.lav.williams@gmail.com>
2020-11-09 11:41:02 -0600

    the text fields populate when you hit edit

--- scripts/JournalFormComponent.js
+++ scripts/JournalFormComponent.js
@@ -2,24 +2,24 @@ import { getEntries, getMoods, saveEntry, useEntries, useMoods } from './Journal
 const contentTarget = document.querySelector(".newEntry")
 const eventHub = document.querySelector(".container")
 
-const render = (moods) => {
+const render = (moods,defaultText) => {
     contentTarget.innerHTML =
         `
 <fieldset>
-                <input type="date" name="Date" id="date">
+                <input type="date" name="Date" id="date" value=${defaultText.date}>
             </fieldset>
             <fieldset>
-                <input type="text" name="concept" id="concept" placeholder="concept">
+                <input type="text" name="concept" id="concept" value=${defaultText.concept}>
             </fieldset>
             <fieldset>
-                <textarea id=newEntry placeholder="Type new Entry Here"></textarea>
+                <textarea id=newEntry placeholder=${defaultText.entry} value=${defaultText.entry}>${defaultText.entry}</textarea>
             </fieldset>
             <fieldset>
                     <select name="mood" id="mood">
-                    ${moods.map(mood=>{
-                        return `<option value=${mood.id}>${mood.label}</option>
+                    ${moods.map(mood => {
+            return `<option value=${mood.id}>${mood.label}</option>
                         `
-                    })}
+        })}
                     
                     </select>
             </fieldset>
@@ -33,7 +33,10 @@ const render = (moods) => {
 eventHub.addEventListener("click", clickEvent => {
 
     if (clickEvent.target.id === "theButton") {
-        const date = document.querySelector("#date").value
+        const id = document.querySelector("#entryId")
+        console.log(id)
+        if (id === "") {
+           const date = document.querySelector("#date").value
         const concept = document.querySelector("#concept").value
         const entry = document.querySelector("#newEntry").value
         const mood = document.querySelector("#mood").value
@@ -45,23 +48,45 @@ eventHub.addEventListener("click", clickEvent => {
             moodId: mood,
         }
         saveEntry(newEntry)
-
         EntryForm()
+        } else {
+            // id value is there, so PUT entry with `updateEntry()`
+            // from data provider
+        }
+       
+        
     }
 
 })
 
+
 export const EntryForm = () => {
     getMoods()
-    .then(()=>{
-        const moods=useMoods()
-        render(moods)
-    })
-    
+        .then(() => {
+            const moods = useMoods()
+            const defaultText={
+                date:"yyyy-MM-dd",
+                concept:"concept",
+                entry:"Type Entry Text Here"
+
+            }
+            render(moods,defaultText)
+        })
+
 }
 
-eventHub.addEventListener("editingEntries", event =>{
-    
-    console.log(event.detail.entryId)
-    
-} )
+eventHub.addEventListener("editingEntries", event => {
+
+    const entrySelected = event.detail.entryId
+    const listOfEntries = useEntries()
+    const entryToEdit = listOfEntries.find(taco => taco.id === parseInt(entrySelected))
+    getMoods()
+        .then(() => {
+            const moods = useMoods()
+            render(moods,entryToEdit)
+
+        })
+
+
+
+})
